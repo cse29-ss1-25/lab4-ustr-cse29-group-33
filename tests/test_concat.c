4d3c6aa2c63f7a8578a237c4107e167303b5938a
Sato <kasato@ieng6-202.ucsd.edu>
2025-07-23 13:06:05 -0700

    lastcommit

--- tests/test_concat.c
+++ tests/test_concat.c
@@ -6,12 +6,24 @@ int main() {
     char input[256];
     while(fgets(input, sizeof(input), stdin)) {
         // Remove newline character if present
-        size_t l = strlen(input);
+        char str1[256];
+	char str2[256];
+
+	UStr s1 = new_ustr(str1);
+	UStr s2 = new_ustr(str2);
+	UStr result = concat(s1, s2);
+
+	printf("Result: '%s' [codepoints: %d, bytes: %d, ascii: %d]\n",
+               result.contents, result.codepoints, result.bytes, result.is_ascii);
+	size_t l = strlen(input);
         if (l > 0 && input[l - 1] == '\n') {
             input[l - 1] = '\0';
         }
 
-        UStr s = new_ustr(input);
-        printf("len(%s) = %d\n", s.contents, len(s));
+        free_ustr(result);
+        free_ustr(s2);
+        free_ustr(s1);
     }
+    return 0;
+   
 }
